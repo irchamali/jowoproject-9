8311b4a206f9935432077ef82cb8da4c9d729e39
irchamali <id.irchamali@gmail.com>
2023-08-02 09:51:58 +0700

    Update minimum requirement for develop.

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -35,7 +35,7 @@ class Database extends Config
 		'hostname' => '127.0.0.1',
 		'username' => 'root',
 		'password' => '',
-		'database' => 'ci4login',
+		'database' => 'pmii_data',
 		'DBDriver' => 'MySQLi',
 		'DBPrefix' => '',
 		'pConnect' => false,
